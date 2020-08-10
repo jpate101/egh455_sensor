2735152e42ad43783194531964ac8ac1eeaaf907
jpate101 <62268987+jpate101@users.noreply.github.com>
2020-08-10 16:32:02 +1000

    Create Sensor.py

---
+++ Egh455_sensor/examples/Sensor.py
@@ -0,0 +1,55 @@
+import time
+from bme280 import BME280
+import ST7735
+from PIL import Image, ImageDraw
+from enviroplus.noise import Noise
+from enviroplus import gas
+import logging
+
+try:
+    from smbus2 import SMBus
+except ImportError:
+    from smbus import SMBus
+
+
+try:
+    # Transitional fix for breaking change in LTR559
+    from ltr559 import LTR559
+    ltr559 = LTR559()
+except ImportError:
+    import ltr559
+
+
+
+
+logging.info(" Sensors data ")
+
+
+bus = SMBus(1)
+bme280 = BME280(i2c_dev=bus)
+noise = Noise()
+
+#temperature, pressure, humidity, light, noise level, gas sensors data 
+while True:
+    temperature = bme280.get_temperature()
+    pressure = bme280.get_pressure()
+    humidity = bme280.get_humidity()
+    lux = ltr559.get_lux()
+    low, mid, high, amp = noise.get_noise_profile()
+    readings = gas.read_all()
+    logging.info(readings)
+
+    logging.info("""Temperature: {:05.2f} *C
+    Pressure: {:05.2f} hPa
+    Relative humidity: {:05.2f} %
+    """.format(temperature, pressure, humidity))
+
+    logging.info("""Light: {:05.02f} Lux
+    """.format(lux))
+
+    logging.info("""Amps: {:05.02f} Lux
+    """.format(amp))
+
+    logging.info(readings)
+
+    time.sleep(3)
