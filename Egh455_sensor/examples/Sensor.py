0e2af2f3ca7bb01126bb223531ec7f456a9a5839
jpate101 <62268987+jpate101@users.noreply.github.com>
2020-08-13 18:45:08 +1000

    Update Sensor.py

--- Egh455_sensor/examples/Sensor.py
+++ Egh455_sensor/examples/Sensor.py
@@ -19,6 +19,9 @@ try:
 except ImportError:
     import ltr559
 
+def convert_gas():
+    pass
+
 logging.basicConfig(
     format='%(message)s',
     level=logging.INFO,
@@ -35,17 +38,25 @@ noise = Noise()
 #temperature, pressure, humidity, light, noise level, gas sensors data 
 while True:
 
-    logging.info(" Sensors data loop ")
+    logging.info(" \n\nSensors data loop \n\n")
 
     temperature = bme280.get_temperature()
     pressure = bme280.get_pressure()
     humidity = bme280.get_humidity()
     lux = ltr559.get_lux()
     low, mid, high, amp = noise.get_noise_profile()
-    readings = gas.read_all()
+    readings = gas.read_all()#.reducing oxidising nh3  carbon monoxide (reducing), nitrogen dioxide (oxidising), and ammonia (NH3)
     logging.info(readings)
 
-    logging.info("""Temperature: {:05.2f} *C
+    #convert readings
+    #red_in_ppm = math.pow(10, -1.25 * math.log10(red_rs/red_r0) + 0.64)
+    #oxi_in_ppm = math.pow(10, math.log10(oxi_rs/oxi_r0) - 0.8129)
+    #nh3_in_ppm = math.pow(10, -1.8 * math.log10(nh3_rs/nh3_r0) - 0.163)
+
+    logging.reducing = math.pow(10, -1.25 * math.log10(1) + 0.64)
+
+    logging.info("""
+    Temperature: {:05.2f} *C
     Pressure: {:05.2f} hPa
     Relative humidity: {:05.2f} %
     """.format(temperature, pressure, humidity))
