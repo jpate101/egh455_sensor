97d6eed5ab4fcd0a487cb6bb123de9e0bc54b644
jpate101 <62268987+jpate101@users.noreply.github.com>
2020-08-13 21:20:34 +1000

    Update Sensor.py

--- Egh455_sensor/examples/Sensor.py
+++ Egh455_sensor/examples/Sensor.py
@@ -65,9 +65,9 @@ while True:
     #nh3_in_ppm = math.pow(10, -1.8 * math.log10(nh3_rs/nh3_r0) - 0.163)
 
     # rs/ro match graph
-    readings.reducing = math.pow(10, -1.25 * math.log10(readings.reducing/158115.24) + 0.64) 
-    readings.oxidising = math.pow(10, math.log10(readings.oxidising/3344) - 0.8129)
-    readings.nh3 = math.pow(10, -1.8 * math.log10(readings.nh3/58920) - 0.163)
+    #readings.reducing = math.pow(10, -1.25 * math.log10(readings.reducing/1) + 0.64) 
+    #readings.oxidising = math.pow(10, math.log10(readings.oxidising/1) - 0.8129)
+    #readings.nh3 = math.pow(10, -1.8 * math.log10(readings.nh3/1) - 0.163)
 
     logging.info("""
     Temperature: {:05.2f} *C
