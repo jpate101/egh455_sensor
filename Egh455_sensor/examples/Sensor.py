377899722738a9758aabe99f0ccc2e1754863a2b
jpate101 <62268987+jpate101@users.noreply.github.com>
2020-08-31 16:12:19 +1000

    Update Sensor.py

--- Egh455_sensor/examples/Sensor.py
+++ Egh455_sensor/examples/Sensor.py
@@ -85,7 +85,7 @@ while True:
     #test2 = noise.get_amplitude_at_frequency_range(0,5000)#https://github.com/pimoroni/enviroplus-python/blob/master/library/enviroplus/noise.py
     #maybe 
     recording = noise._record()
-    magnitude = numpy.abs(recording[:, 0])
+    magnitude = numpy.abs(recording[:])
     test2 = numpy.max(magnitude[:])
     #return numpy.mean(magnitude[start:end])#replace mean with max
 
