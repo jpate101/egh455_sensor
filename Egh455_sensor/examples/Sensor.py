fb41d63c94f4ae1309f7b53f5397e1643b2038b1
jpate101 <62268987+jpate101@users.noreply.github.com>
2020-09-03 22:11:54 +1000

    Update Sensor.py

--- Egh455_sensor/examples/Sensor.py
+++ Egh455_sensor/examples/Sensor.py
@@ -103,11 +103,8 @@ while True:
             dtype='float64'
         )
     test3 = numpy.abs(test3[:])
-
-    test6 = test3
-
     test3 = numpy.max(test3[:])
-    test4 = 20*math.log10((test3+5)) 
+    test4 = 20*math.log10((test3).000002) 
     test5 = 20*math.log10((test3)/.006) 
 
     recording = noise._record()
