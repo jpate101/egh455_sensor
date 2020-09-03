87844c2eacc5324884fdf60ebf217e01949c8328
jpate101 <62268987+jpate101@users.noreply.github.com>
2020-09-03 21:06:22 +1000

    Update Sensor.py

--- Egh455_sensor/examples/Sensor.py
+++ Egh455_sensor/examples/Sensor.py
@@ -89,17 +89,15 @@ while True:
     #maybe 
 
     test3 = sounddevice.rec(
-            int(1 * 16000),
+            int(2 * 16000),
             samplerate= 16000,
             blocking=True,
             channels=1,
             dtype='float64'
         )
-    test3_1 = numpy.abs(test3[:])
-    test3 = numpy.max(test3_1[:])
-
-    test4 = 20*math.log10(test3/.00015) 
-    test5 = 20*math.log10(test3/.006) 
+    test3 = numpy.abs(test3[:])
+    test3 = numpy.max(test3[:])
+    test4 = 20*math.log10(test3/.000003) 
 
     recording = noise._record()
     magnitude = numpy.abs(recording[:])
@@ -209,8 +207,6 @@ while True:
     Test4: {:05.08f} DB
     Test5: {:05.08f} DB
     """.format(DB,test2,test3,test4,test5))
-
-    print(str(test3_1))
 
     time.sleep(2)
     #teest
