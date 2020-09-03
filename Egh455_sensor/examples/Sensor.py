c82d89b9681af7a27b85173a2d3f8447508022ae
jpate101 <62268987+jpate101@users.noreply.github.com>
2020-09-03 23:02:15 +1000

    Update Sensor.py

--- Egh455_sensor/examples/Sensor.py
+++ Egh455_sensor/examples/Sensor.py
@@ -99,7 +99,10 @@ while True:
 
     recording = noise._record()
     magnitude = numpy.abs(recording[:])
-    test2 = numpy.max(magnitude[:])
+
+    magnitude[argsort(magnitude)[-1000:]]
+    
+    test2 = numpy.mean(magnitude[:])
     test3 = 65*math.log10((test2)/.03) 
     test4 = 20*math.log10((test2))+60 
     DB = math.pow(10, 3.6 * math.log10(mid) + 3.35)
