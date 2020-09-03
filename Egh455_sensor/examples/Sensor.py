f4883f8b8dd78af3547b67da44fc002af0e4a7f7
jpate101 <62268987+jpate101@users.noreply.github.com>
2020-09-04 00:30:51 +1000

    Update Sensor.py

--- Egh455_sensor/examples/Sensor.py
+++ Egh455_sensor/examples/Sensor.py
@@ -101,7 +101,7 @@ while True:
     magnitude = numpy.abs(recording[:])
 
     #z = magnitude[-100:]
-    z = numpy.sort(magnitude)[7900:]
+    z = numpy.sort(magnitude)[-100:]
     print(str(len(z)))
     print(str((z)))
 
