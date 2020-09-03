0dcfe24b355e180af8ef8744526495f24b44be1a
jpate101 <62268987+jpate101@users.noreply.github.com>
2020-09-04 00:28:36 +1000

    Update Sensor.py

--- Egh455_sensor/examples/Sensor.py
+++ Egh455_sensor/examples/Sensor.py
@@ -105,7 +105,7 @@ while True:
     print(str(len(z)))
     print(str((z)))
 
-    test2 = numpy.mean(z[:])
+    test2 = numpy.max(magnitude[:])
     test3 = 65*math.log10((test2)/.03) 
     test4 = 20*math.log10((test2))+60 
     DB = math.pow(10, 3.6 * math.log10(mid) + 3.35)
