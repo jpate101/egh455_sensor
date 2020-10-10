7770feaf4f24245c5e8a8ae40402c4d8622b2e7c
jpate101 <62268987+jpate101@users.noreply.github.com>
2020-10-10 12:42:56 +1000

    Update Sensor.py

--- Egh455_sensor/examples/Sensor.py
+++ Egh455_sensor/examples/Sensor.py
@@ -55,7 +55,7 @@ VO_OX = 0
 VO_NH3 = 0
 
 logging.info(" \ndetermine baseline Vo(clean air) values")
-sample_size = 100
+sample_size = 300
 for x in range(sample_size):
     readings = gas.read_all()
     time.sleep(.01)
